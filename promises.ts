e29fad1d63d8f6db572abfb52d390f84b2e35557
Lars <lars@recap.dk>
2025-09-09 14:20:24 +0200

    Made the last changes. code fully functional

--- promises.ts
+++ promises.ts
@@ -1,19 +1,54 @@
 const flipCoin = () => {
   return new Promise((resolve, reject) => {
-    const result = Math.random() < 0.5;
-    if (result === true) {
-      fetch("https://api.adviceslip.com/advice")
-        .then((response) => response.json())
-        .then((data) => {
-          resolve(data.slip.advice);
-        });
+    let result = Math.random();
+    if (result > 0.5) {
+      resolve("You Win!");
     } else {
-      reject("No advice today. You have to learn life the hard way.");
+      reject("You Loose");
     }
   });
 };
 
-flipCoin()
+const coinFlipResult = async () => {
+  try {
+    const advice = await flipCoin();
+    console.log(advice);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//coinFlipResult();
+
+const fetchAdvice = async () => {
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    console.log(data.slip.advice);
+  } catch (error) {
+    console.log("Error fetching the advice:", error);
+  }
+};
+
+const flipCoinWithAdvice = async () => {
+  try {
+    const result = await flipCoin();
+    if (result === "You Win!") {
+      fetchAdvice();
+    } else {
+      console.log(result);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+flipCoinWithAdvice();
+
+//fetchAdvice();
+
+//
+/*
   .then((advice) => {
     console.log("Advice:", advice);
   })
@@ -47,4 +82,21 @@ fetch= ('https://api.adviceslip.com/advice')
 .catch(error => {
     console.error('Error fetching the advice:', error));
 });
+
+
+
+const flipCoin = () => {
+  return new Promise((resolve, reject) => {
+    const result = Math.random() < 0.5;
+    if (result === true) {
+      fetch("https://api.adviceslip.com/advice")
+        .then((response) => response.json())
+        .then((data) => {
+          resolve(data.slip.advice);
+        });
+    } else {
+      reject("No advice today. You have to learn life the hard way.");
+    }
+  });
+};
 */
